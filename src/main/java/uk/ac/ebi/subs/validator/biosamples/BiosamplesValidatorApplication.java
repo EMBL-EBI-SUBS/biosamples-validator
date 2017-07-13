174cbe6b2f3a1845aaa99ffd5d8b64fa70ae9831
Flavia Penim <flaviapenim@gmail.com>
2017-07-13 17:03:25 +0100

    Add cd configuration.

--- src/main/java/uk/ac/ebi/subs/validator/biosamples/BiosamplesValidatorApplication.java
+++ src/main/java/uk/ac/ebi/subs/validator/biosamples/BiosamplesValidatorApplication.java
@@ -2,16 +2,17 @@ package uk.ac.ebi.subs.validator.biosamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.converter.MessageConverter;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "uk.ac.ebi.subs.validator")
 public class BiosamplesValidatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BiosamplesValidatorApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(BiosamplesValidatorApplication.class);
+        ApplicationPidFileWriter applicationPidFileWriter = new ApplicationPidFileWriter();
+        springApplication.addListeners( applicationPidFileWriter );
+        springApplication.run(args);
     }
 }
